c1cc528f0f4da185f6fbb53c7f04ad3fd538b23d
Salman Javaid <javaid.salman@gmail.com>
2013-09-15 17:15:35 +0500

    templates_13

--- DataStructures/LinkedList/Singly_Linked_List/LinkedList.h
+++ DataStructures/LinkedList/Singly_Linked_List/LinkedList.h
@@ -14,3 +14,55 @@ class LinkedList
   void Print();
   ~LinkedList();
 };
+
+
+template<class T>
+LinkedList<T>::LinkedList()
+{
+  head = 0;
+  tail = 0;
+}
+
+template<class T>
+void LinkedList<T>::Insert(T num)
+{
+  if (head == 0)
+    {
+      Node<T> *head_node = new Node<T>();
+      head_node->SetVal(num);
+      head = head_node;
+      tail = head_node;
+      tail->next = head_node;
+      head->next = tail;
+    }
+  else
+    {
+      Node<T> *head_node = new Node<T>();
+      head_node->SetVal(num);
+      tail->next = head_node;
+      tail = head_node;
+    }
+
+}
+
+template<class T>
+void LinkedList<T>::Print()
+{
+  Node<T> *temp = head;
+  while(temp != 0)
+    {
+      printf("%i\n", temp->GetVal(0));
+      temp = temp->next;
+    }
+}
+
+template<class T>
+LinkedList<T>::~LinkedList()
+{
+
+
+}
+
+
+
+
--- DataStructures/LinkedList/Singly_Linked_List/Node.h
+++ DataStructures/LinkedList/Singly_Linked_List/Node.h
@@ -10,3 +10,23 @@ class Node
   T GetVal(T);
   ~Node();
 };
+
+
+
+template<class T>
+Node<T>::Node()
+{
+  val = 0;
+}
+
+template<class T>
+void Node<T>::SetVal(T Val)
+{
+  val = Val;
+}
+
+template<class T>
+T Node<T>::GetVal(T Index)
+{
+  return val;
+}
