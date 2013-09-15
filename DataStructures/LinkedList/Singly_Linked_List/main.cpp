915f24098f8e7a0bada39a08a7cbc7467c3fb26e
Salman Javaid <javaid.salman@gmail.com>
2013-09-15 18:13:13 +0500

    Destructors_3

--- DataStructures/LinkedList/Singly_Linked_List/main.cpp
+++ DataStructures/LinkedList/Singly_Linked_List/main.cpp
@@ -5,10 +5,10 @@ int main()
   LinkedList<int> L_1;
 
   L_1.Insert(5);
-  // L_1.Insert(10);
-  // L_1.Insert(15);
-  // L_1.Insert(20);
-  // L_1.Print();
-
+  L_1.Insert(10);
+  L_1.Insert(15);
+  L_1.Insert(20);
+  L_1.Print();
+  delete []L_1;
   return 0;
 }
