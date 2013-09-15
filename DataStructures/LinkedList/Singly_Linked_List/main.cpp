ad66b56a70345388e1c77e35c1c70603b8764311
Salman Javaid <javaid.salman@gmail.com>
2013-09-15 11:03:21 +0500

    templates_11

--- DataStructures/LinkedList/Singly_Linked_List/main.cpp
+++ DataStructures/LinkedList/Singly_Linked_List/main.cpp
@@ -4,11 +4,11 @@ int main()
 {
   LinkedList<int> L_1;
 
-  L_1.Insert(5);
-  L_1.Insert(10);
-  L_1.Insert(15);
-  L_1.Insert(20);
-  L_1.Print();
+  // L_1.Insert(5);
+  // L_1.Insert(10);
+  // L_1.Insert(15);
+  // L_1.Insert(20);
+  // L_1.Print();
 
   return 0;
 }
