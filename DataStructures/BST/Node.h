05fa19e77c4fc6d53e853b532e652ed2d514404d
Salman Javaid <javaid.salman@gmail.com>
2013-09-17 10:07:07 +0500

    BST_Node Header Update_1

---
+++ DataStructures/BST/Node.h
@@ -0,0 +1,43 @@
+template<class T>
+class Node
+{
+ private:
+  T val;
+ public:
+  Node *left;
+  Node *right;
+  Node();
+  void SetVal(T);
+  T GetVal();
+  ~Node();
+}
+
+
+template<class T>
+Node<T>::Node()
+{
+  left = 0;
+  right = 0;
+}
+
+
+template<class T>
+void Node<T>::SetVal(T Val)
+{
+  val = Val;
+}
+
+
+template<class T>
+T Node<T>::GetVal()
+{
+  return val;
+}
+
+template<class T>
+Node<T>::~Node()
+{
+
+}
+
+
